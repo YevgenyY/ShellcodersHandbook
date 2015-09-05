3a775c72035b23906ab3d4485ba98d8fe7a68b5b
Yevgeny V.Yorkhov <yy@ixi.ru>
2015-09-06 01:18:11 +0300

    Add some shellcode tricks

--- ch03/wack.c
+++ ch03/wack.c
@@ -1,7 +1,19 @@
 #include <stdint.h>
+#if ELF32 
 char shellcode[] = "\xbb\x00\x00\x00\x00"           
                    "\xb8\x01\x00\x00\x00"                  
                    "\xcd\x80";                  
+#endif
+#if LONG_ZERO_STRINGS
+char shellcode[] = "\x48\xbb\x00\x00\x00\x00\x00\x00\x00\x00"           
+                   "\x48\xb8\x01\x00\x00\x00\x00\x00\x00\x00"
+                   "\xcd\x80";                  
+#else
+char shellcode[] = "\x48\x31\xdb"
+								   "\xb0\x01"
+									 "\xcd\x80"
+									 "\x48\x31\xdb";
+#endif
 
 int main()
 {
